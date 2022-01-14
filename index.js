2d9e82b67ea9b1fb3c3a4db64c7e3740342c5bec
Shubham Aher <ahers247@gmail.com>
2022-01-14 17:17:52 +0530

    Finall Project

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
+let input=0;
+function fun1(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"1";
 
+}
+function fun2(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"2";
+
+}
+function fun3(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"3";
+
+}
+function fun4(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"4";
+
+}
+function fun5(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"5";
+
+}
+function fun6(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"6";
+
+}
+function fun7(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"7";
+
+}
+function fun8(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"8";
+
+}
+function fun9(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"9";
+
+}
+function fun0(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"0";
+
+}
+function add(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"+";
+
+}
+function min(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"-";
+
+}
+function mul(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"*";
+
+}
+function div(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+"/";
+
+}
+function dot(){
+    const inp= document.getElementById("input");
+    inp.value= inp.value+".";
+
+}
+function clr(){
+    const inp= document.getElementById("input");
+    inp.value= "";
+
+}
+function ans(){
+    const inp= document.getElementById("input");
+    inp.value=eval(inp.value);
+
+}
